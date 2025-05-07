3ef447574d4c3d89585bcb2aed1afd11430ed428
조영호 <aeternum9342@gmail.com>
2025-05-07 22:11:44 +0900

    오브젝트 설계 원칙편 5-2 예제

---
+++ src/main/java/org/eternity/event/problem/Schedule.java
@@ -0,0 +1,51 @@
+package org.eternity.event.problem;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+
+public class Schedule {
+    private static final int DAYS_IN_WEEK = 7;
+
+    private String title;
+    private LocalTime from;
+    private Duration duration;
+
+    private Integer ordinal;
+    private DayOfWeek dayOfWeek;
+
+    private Set<DayOfWeek> dayOfWeeks;
+
+    public Schedule(String title, LocalTime from, Duration duration, Integer ordinal, DayOfWeek dayOfWeek) {
+        this.title = title;
+        this.from = from;
+        this.duration = duration;
+        this.dayOfWeek = dayOfWeek;
+        this.ordinal = ordinal;
+    }
+
+    public Schedule(String title, LocalTime from, Duration duration, Set<DayOfWeek> dayOfWeeks) {
+        this.title = title;
+        this.from = from;
+        this.duration = duration;
+        this.dayOfWeeks = dayOfWeeks;
+    }
+
+    public boolean includes(LocalDate day) {
+        if (dayOfWeek != null) {
+            if (!day.getDayOfWeek().equals(dayOfWeek)) {
+                return false;
+            }
+
+            return (day.getDayOfMonth() / DAYS_IN_WEEK) + 1 == ordinal;
+        }
+
+        if (!dayOfWeeks.isEmpty()) {
+            return dayOfWeeks.contains(day.getDayOfWeek());
+        }
+
+        return false;
+    }
+}
---
+++ src/test/java/org/eternity/event/problem/ScheduleTest.java
@@ -0,0 +1,18 @@
+package org.eternity.event.problem;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ScheduleTest {
+    @Test
+    public void includes() {
+        Schedule schedule = new Schedule("미팅", LocalTime.of(13, 0), Duration.ofHours(1), 2, DayOfWeek.MONDAY);
+        assertThat(schedule.includes(LocalDate.of(2024, 7, 8))).isTrue();
+    }
+}
